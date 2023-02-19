bdefc293bebb0cd401f8832e2d79168a78123118
asmfahim <abusmfahimf9@gmail.com>
2023-02-20 04:50:36 +0600

    add enqueue scripts in proper way and screenshot.png file add

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 
 function boss_enqueue_scripts(){
-    wp_enqueue_style( 'stylesheet', get_stylesheet_uri( ), [], filemtime(get_template_directory(  ).'/style.css'),'all' );
+    //It is a good practice use in wp_register_(style/script) you can also condition this procese
+    //This is Style.css
+    wp_register_style('style-css', get_stylesheet_uri( ), [], filemtime(get_template_directory(  ).'/style.css'),'all' );  
+    //This is main.js
+    wp_register_script( 'main-js', get_template_directory_uri(  ).'/assets/main.js', [], filemtime(get_template_directory(  ).'/assets/main.js'), true ); 
+
+    wp_enqueue_style( 'style-css' );
+    wp_enqueue_script( 'main-js' );
+
+
 }
 add_action( 'wp_enqueue_scripts','boss_enqueue_scripts' ); // This hooks for all links and scripts

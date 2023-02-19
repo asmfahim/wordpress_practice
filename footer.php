cac83f081325dc6716df92a05b6d8df19e5f6ccf
asmfahim <abusmfahimf9@gmail.com>
2023-02-20 03:20:40 +0600

    Add wp_head & wp_footer funciton

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Footer template file
+ * 
+ * @package Boss 
+ */
+?>
+
+<footer>Footer</footer>   
+<?php wp_footer(  ); ?> 
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,18 +2,13 @@
 /**
  * Main template file
  * 
- *@package boss
+ *@package Boss
  */
+get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+    <div>Content</div>
+   
+<?php 
+get_footer( ); 

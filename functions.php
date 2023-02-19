aef8468a06410db7ca9f3e1387c196d7cf4c0723
asmfahim <abusmfahimf9@gmail.com>
2023-02-20 04:27:27 +0600

    add scripts and stylesheet

--- functions.php
+++ functions.php
@@ -1,1 +1,6 @@
 <?php
+
+function boss_enqueue_scripts(){
+    wp_enqueue_style( 'stylesheet', get_stylesheet_uri( ), [], filemtime(get_template_directory(  ).'/style.css'),'all' );
+}
+add_action( 'wp_enqueue_scripts','boss_enqueue_scripts' ); // This hooks for all links and scripts

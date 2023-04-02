81a387f0293d1ac99fa0fea28f5c5c68832fd7d6
asmfahim <abusmfahimf9@gmail.com>
2023-04-03 03:01:24 +0600

    post show with grid view

--- index.php
+++ index.php
@@ -7,8 +7,64 @@
 get_header();
 ?>
 
+    <div id="primary">
+        <main id="main" class="site-main mt-3" role="main">
+            <?php
+                if(have_posts(  )){
+                    ?>
+                        <div class="container">
+                            <?php
+                                if( is_home(  ) && ! is_front_page(  ) ){
+                                    ?>
+                                        <div class="mb-5">
+                                            <h1 class="page-title sr-only sr-only-focusable">
+                                                <?php single_post_title( ); ?>
+                                            </h1>
+                                        </div>
+                                    <?php
+                                }
+                             ?>
 
-    <div>Content</div>
+
+                            <div class="row">
+                            <?php
+                                $index = 0; //index
+                                $no_of_col = 3;
+                                while (have_posts(  ) ): the_post(  ); 
+
+                                if(0 === $index % $no_of_col){
+                                    ?>
+                                        <div class=" col-lg-4 col-md-6 col-sm-12">
+                                            
+                                    <?php
+                                         }
+                                    ?>
+                                        <h4><?php the_title( ); ?></h4>
+                                        <div><?php the_excerpt(  ); ?> </div>
+
+                                    <?php
+                                        $index +=1;
+
+                                    if(0 !== $index && 0 === $index % $no_of_col ){
+                                        ?>
+                                        </div>
+                                        <?php
+                                    }
+
+                                    // the_title(  );
+                                    // echo '</br></br>';
+                                    // the_excerpt(  );
+                                    // echo '</br></br>................................ </br>';
+                                endwhile;
+                            ?>
+                            </div>
+                        </div>
+
+                    <?php 
+                } 
+            ?>
+        </main>
+    </div>
    
 <?php 
 get_footer( ); 

a070fe24882a01ecc79f4e6de2c52e72688251f8
asmfahim <abusmfahimf9@gmail.com>
2023-07-15 00:26:31 +0600

    add nonce to protect csrf

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -39,6 +39,10 @@ public function add_custom_meta_box(){
 
 public function custom_meta_box_html( $post ){
     $value = get_post_meta( $post->ID , '_hide_page_title', true );
+    /**
+     * use nonce for varefication
+     */
+    wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
 
     ?>
     <label for="boss-field"> <?php esc_html_e('Hide the page title','boss'); ?> </label>
@@ -59,6 +63,22 @@ public function custom_meta_box_html( $post ){
 }
 
 public function save_post_meta_data($post_id ){
+    /**
+     * When the post is saved or upload we get $_POST available
+     * check the current user is authorised
+     */
+    if( ! current_user_can('edit_post',$post_id)){
+        return;
+    }
+    /**
+     * check the nonce value we recived is the same we created or not
+     */
+    if(! isset($_POST['hide_title_meta_box_nonce_name']) || 
+        ! wp_verify_nonce( $_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__))
+    ){
+        return;
+    }
+
     if ( array_key_exists( 'boss_hide_title_field', $_POST ) ) {
         update_post_meta(
             $post_id,
